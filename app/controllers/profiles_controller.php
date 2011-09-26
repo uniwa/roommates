d7a8d0bc70a4735315654b77704a9d4a4c226362
Chrysanthi Alexiou <ubichrys@gmail.com>
2011-09-26 14:28:51 +0300

    o foititis kataxwrei to profil tou, to kanei edit kai delete

--- app/controllers/profiles_controller.php
+++ app/controllers/profiles_controller.php
@@ -12,5 +12,36 @@ class ProfilesController extends AppController {
         $this->set('profile', $this->Profile->read());
     }
 
+    		function add(){
+			if (!empty($this->data)) {
+				if ($this->Profile->save($this->data)){
+					$this->Session->setFlash('Profile has been added');
+					$this->redirect(array('action' => 'index'));
+				}
+			}
+		}	
+
+	
+		function delete($id){
+			if ($this->Profile->delete($id)){
+				$this->Session->setFlash('Profile deleted');
+				$this->redirect(array('action'=> 'index'));
+			}
+		}
+
+
+		function edit($id = null){
+			$this->Profile->id = $id;
+			if (empty($this->data)){
+				$this->data = $this->Profile->read();
+			}else {
+				if ($this->Profile->save($this->data)){
+					$this->Session->setFlash('Profile updated.');
+					$this->redirect(array('action'=> 'index'));
+				}
+			}
+		}
+
+
 }
 ?>
