e77b17decdb0eead007058814b03872788c896f5
Thanasis Karetsos <karetsosjr@gmail.com>
2011-09-26 09:14:37 +0300

    updated houses controller
    - renamed controller file
    - changed the variables' names

---
+++ app/controllers/houses_controller.php
@@ -0,0 +1,43 @@
+<?php
+class HousesController extends AppController {
+
+    var $name = 'Houses';
+
+    function index() {
+        $this->set('houses', $this->House->find('all'));
+    }
+
+    function view($location = null) {
+        $this->House->location = $location;
+        $this->set('house', $this->House->read());
+    }
+
+    function add() {
+        if (!empty($this->data)) {
+            if ($this->House->save($this->data)) {
+                $this->Session->setFlash('Your house has been saved.');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
+
+    function delete($id) {
+        $this->House->delete($id);
+        $this->Session->setFlash('The house with id: '.$id.' has been deleted.');
+        $this->redirect(array('action'=>'index'));
+    }
+
+    function edit($id = null) {
+        $this->House->id = $id;
+        if (empty($this->data)) {
+            $this->data = $this->House->read();
+        }
+        else {
+            if ($this->House->save($this->data)) {
+                $this->Session->setFlash('This house has been updated.');
+                $this->redirect(array('action' => 'index'));
+            }
+        }
+    }
+}
+?>
