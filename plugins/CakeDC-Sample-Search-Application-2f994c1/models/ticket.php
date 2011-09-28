e2c6296232aaa07e8ebf15c662c3fb0d68e67493
fotis <fotis@laptop.(none)>
2011-09-28 12:07:12 +0300

    default.ctp - global.css

---
+++ plugins/CakeDC-Sample-Search-Application-2f994c1/models/ticket.php
@@ -0,0 +1,78 @@
+<?php
+class Ticket extends AppModel {
+/**
+ * Name
+ *
+ * @var string $name
+ */
+	public $name = 'Ticket';
+
+/**
+ * Validation parameters - initialized in constructor
+ *
+ * @var array
+ */ 
+	public $validate = array(
+		'status' => array(
+			'notempty' => array(
+				'rule' => array('notempty'))),
+		'category' => array(
+			'notempty' => array(
+				'rule' => array('notempty'))),
+		'title' => array(
+			'notempty' => array(
+				'rule' => array('notempty'))));
+
+/**
+ * Behaviors
+ *
+ * @var array
+ * @access public
+ */
+	public $actsAs = array(
+		'Search.Searchable');
+
+/**
+ * Field names accepted for search queries.
+ *
+ * @var array
+ * @see SearchableBehavior
+ */
+	public $filterArgs = array(
+		array('name' => 'title', 'type' => 'query', 'method' => 'filterTitle'),
+		array('name' => 'status', 'type' => 'string'),
+	);
+		
+/**
+ * Constructor
+ *
+ * @param mixed $id Model ID
+ * @param string $table Table name
+ * @param string $ds Datasource
+ * @access public
+ */
+	public function __construct($id = false, $table = null, $ds = null) {
+		$this->statuses = array(
+			'open' => __('Open', true),
+			'closed' => __('Closed', true));
+		$this->categories = array(
+			'bug' => __('Bug', true),
+			'support' => __('Support', true),
+			'technical' => __('Technical', true),
+			'other' => __('Other', true));
+		parent::__construct($id, $table, $ds);
+	}
+	
+	public function filterTitle($data, $field = null) {
+		if (empty($data['title'])) {
+			return array();
+		}
+		$title = '%' . $data['title'] . '%';
+		return array(
+			'OR'  => array(
+				$this->alias . '.title LIKE' => $title,
+				$this->alias . '.content LIKE' => $title,
+			));
+	}
+	
+}
