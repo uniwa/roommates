495d419729ba8ed723edd3239543bf72e3fdb42b
Konstantinos Maragos <kmaragos@gmail.com>
2011-09-30 11:04:37 +0300

    add 'has house' condition in profile search

--- app/controllers/profiles_controller.php
+++ app/controllers/profiles_controller.php
@@ -5,6 +5,7 @@ class ProfilesController extends AppController {
     var $components = array('RequestHandler');
  //   var $profile = 
  //   var $preferences = 
+//	var $uses = array('House');
 
     function index() {
         if ($this->RequestHandler->isRss()) {
@@ -72,6 +73,12 @@ class ProfilesController extends AppController {
 
         // set the conditions
         $searchconditions = array('Profile.visible' => 1);
+
+		if(!empty($this->data['User']['hasHouse'])){
+			$ownerId = $this->Profile->House->find('all', array('fields' => 'DISTINCT profile_id'));
+			$ownerId = Set::extract($ownerId, '/House/profile_id');
+			$searchconditions['Profile.id'] = $ownerId;
+		};
 
         if(!empty($searchArgs['agemin'])) {
             //$searchconditions['Profile.age >'] = $searchArgs['agemin'];
