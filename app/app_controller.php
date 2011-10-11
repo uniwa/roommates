825ecf72ea7500049b362c652c2df086c84a4f6d
Thanos Demertzis <thandem88@gmail.com>
2011-10-11 11:37:21 +0300

    Set Greek login error message

--- app/app_controller.php
+++ app/app_controller.php
@@ -3,5 +3,11 @@ class  AppController extends Controller{
 
 	var $components = array('Auth', 'Session');
 	var $Helpers  = Array('Html', 'Form', 'Session');
+
+	function beforeFilter(){
+
+		$this->Auth->loginError = "Δώστε έγκυρο όνομα χρήστη και συνθηματικό.";
+		$this->Auth->authError = "Δώστε έγκυρο όνομα χρήστη και συνθηματικό.";
+	}
 }
 ?>
