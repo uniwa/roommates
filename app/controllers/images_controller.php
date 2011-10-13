44d13c8d3cc06f052717bf112422091726306568
Tasos Latsas <tlatsas2000@gmail.com>
2011-10-13 13:06:11 +0300

    fix missing parenthesis

--- app/controllers/images_controller.php
+++ app/controllers/images_controller.php
@@ -15,7 +15,7 @@ class ImagesController extends AppController {
         }
         if ( $this->imageCount($id) >= 5 ) {
             $this->Session->setFlash(__('Έχετε συμπληρώσει τον μέγιστο επιτρεπτό αριθμό φωτογραφιών'));
-            $this->redirect($this->referer();
+            $this->redirect($this->referer());
         }
 		if(!empty($this->data)) {
 			$this->Image->create();
