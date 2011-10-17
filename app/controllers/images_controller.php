8ffad75e6ff5bb5ecc29bd1c76db3e676ea5a0ae
Tasos Latsas <tlatsas2000@gmail.com>
2011-10-17 11:58:01 +0300

    remove capita extension
    magic files return lowercase extensions

--- app/controllers/images_controller.php
+++ app/controllers/images_controller.php
@@ -106,7 +106,7 @@ class ImagesController extends AppController {
 
     private function validType($file) {
         /* check if uploaded image is a valid filetype */
-        $valid_types = array("png", "jpg", "jpeg", "PNG", "JPG", "JPEG");
+        $valid_types = array("png", "jpg", "jpeg");
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $mime_type = finfo_file($finfo, $file);
         $mime_type = explode("/", $mime_type);
