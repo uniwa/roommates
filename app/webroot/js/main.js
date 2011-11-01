20a7510c27a42469ffb6e4ee12d3ac2b660141eb
Tasos Latsas <tlatsas2000@gmail.com>
2011-11-01 12:59:26 +0200

    hide 'delete' action for default image in house view

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -13,12 +13,22 @@ $(function(){
     });
 
 
+    // image actions for image list
     $(".imageactions").css("visibility", "hidden");
-    $(".liimage").hover(    
+    $(".liimage").hover(
         function(){
             $(this).find('.imageactions').css("visibility", "visible");
         },
         function(){
             $(".imageactions").css("visibility", "hidden");
         });
+    // image actions for default image
+    $(".default-image").hover(
+        function(){
+            $(this).find('.imageactions').css("visibility", "visible");
+        },
+        function(){
+            $(".imageactions").css("visibility", "hidden");
+        });
+
 });
