3a5bdc1c978d20c180cfdb6dee8d984fc8baec14
Chrysanthi Alexiou <ubichrys@gmail.com>
2011-11-14 11:23:42 +0200

    *Done with query *Added send-mail-to-users stuff (have to check if that works)

--- vendors/shells/email.php
+++ vendors/shells/email.php
@@ -2,6 +2,7 @@
 class EmailShell extends Shell{
 
     var $uses = array('House', 'Preference', 'User', 'Profile');
+    
     function main(){
         
         $today = date('Y-m-d', strtotime("+1 day")); //because daysAsSql returns yesterday, use strtotime
@@ -18,18 +19,18 @@ class EmailShell extends Shell{
         //get houses created or modified today
         $houses = $this->House->find('all', array('conditions' => $conditions));
         //pr($houses);die();  //House, HouseType, Floor, HeatingType, Municipality, User, Image 
-        //if (isset($houses)) count($houses);
 
 
         //get (only) users data and preferences 
         $users = $this->Profile->find('all', array('conditions' => array( 'User.role' => 'user' )));
         //pr($users);die(); //User, Preference, Profile 
-        //if (isset($users)) count($users);
 
-        $compatible_houses = array();
         $email_users = array();
 
         for ($i=0; $i<count($users); $i++){
+
+            $compatible_houses = array();
+            
             for($j=0; $j<count($houses); $j++){
                 
                 if(    ( $this->compare_min($houses[$j]['House']['area'], $users[$i]['Preference']['area_min']))
@@ -38,10 +39,10 @@ class EmailShell extends Shell{
                     && ( $this->compare_max($houses[$j]['House']['price'], $users[$i]['Preference']['price_max']))
                     && ( $this->compare_min($houses[$j]['House']['floor_id'], $users[$i]['Preference']['floor_id_min']) )
             
-                    && ( $this->compare_pref_min($houses[$j]['House']['bathroom_num'], $users[$i]['Preference']['bathroom_num_min'])) //not comp
-                    && ( $this->compare_pref_min($houses[$j]['House']['construction_year'], $users[$i]['Preference']['construction_year_min']) ) //not comp
-                    && ( $this->compare_pref_min($houses[$j]['House']['rent_period'], $users[$i]['Preference']['rent_period_min']) ) //not comp
-                    && ( $this->compare_checkbox_null($houses[$j]['House']['solar_heater'], $users[$i]['Preference']['pref_solar_heater']) ) //not comp
+                    && ( $this->compare_pref_min($houses[$j]['House']['bathroom_num'], $users[$i]['Preference']['bathroom_num_min']))
+                    && ( $this->compare_pref_min($houses[$j]['House']['construction_year'], $users[$i]['Preference']['construction_year_min']) )
+                    && ( $this->compare_pref_min($houses[$j]['House']['rent_period'], $users[$i]['Preference']['rent_period_min']) )
+                    && ( $this->compare_checkbox_null($houses[$j]['House']['solar_heater'], $users[$i]['Preference']['pref_solar_heater']) )
                     && ( $this->compare_checkbox_null($houses[$j]['House']['aircondition'], $users[$i]['Preference']['pref_aircondition']) )
                     && ( $this->compare_checkbox_null($houses[$j]['House']['garden'], $users[$i]['Preference']['pref_garden']) )
                     && ( $this->compare_checkbox_null($houses[$j]['House']['parking'], $users[$i]['Preference']['pref_parking']) )
@@ -51,7 +52,7 @@ class EmailShell extends Shell{
                     && ( $this->compare_equal($houses[$j]['House']['house_type_id'], $users[$i]['Preference']['pref_house_type_id']))
                     && ( $this->compare_equal($houses[$j]['House']['heating_type_id'], $users[$i]['Preference']['pref_heating_type_id']))
                     
-                    && ( $this->compare_equal_null($houses[$j]['House']['municipality_id'], $users[$i]['Preference']['pref_municipality'])) //not OK
+                    && ( $this->compare_equal_null($houses[$j]['House']['municipality_id'], $users[$i]['Preference']['pref_municipality']))
                     && ( $this->shared_pay($houses[$j]['House']['shared_pay'], $users[$i]['Preference']['pref_shared_pay']) )
                     
                     && ( $this->compare_date_min($houses[$j]['House']['availability_date'], $users[$i]['Preference']['availability_date_min']) )
@@ -68,71 +69,123 @@ class EmailShell extends Shell{
             }
             $email_users[$users[$i]['Profile']['email']] = $compatible_houses;
         }
-
-        pr($email_users);die();
+        
+        $this->email($email_users);
     }
 
+        //refers to the compulsory field for the owner's house (availability_date)
+        //default in Preferences --> 0000-00-00
         private function compare_date_min($attr, $pref){
-            if( strtotime($attr) <= strtotime($pref)){
-                return true;    
+            if ($pref == "0000-00-00"){
+                return true;
             }else{
-                return false;    
+                if( strtotime($attr) <= strtotime($pref)){
+                    return true;    
+                }else{
+                    return false;    
+                }
             }
         }
 
+        //refers to optional checkbox fields for the owner's house (solar_heater, air_condition, garden, parking, security_doors, storeroom)
+        //default in Preferences --> NULL
+        //default in Houses --> NULL
         private function compare_checkbox_null($attr, $pref){
-            if (isset($attr)){ 
+            if( isset($pref) && ($pref) ){
+                if ( isset($attr) && ($attr) ){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
                 return true;
-            }else if ( !isset($attr) && !isset($pref) ){ 
-                return true;
-            }else if ( !isset($attr) && isset($pref) ){
-                return false;
             }
         }
 
-        private function has_photo($attr, $pref){
-            if (!isset($attr) && ($pref)){
-                return false;
+
+
+        //refers to optional checkbox fields (furnitured, disability_facilities)  
+        //default in Preferences --> 2, 0
+        //default in Houses --> NULL
+        private function compare_checkbox($attr, $pref){
+            if($pref == 1){
+                if (isset($attr) && ($attr) ){
+                    return true;
+                }else{
+                    return false;
+                }
             }else{
                 return true;    
             }
         }
 
 
-        private function compare_checkbox($attr, $pref){
-            if (!isset($attr) && ($pref == 1) ){ 
-                return false;
+        //refers to optional checkbox field shared_pay  
+        //default in Preferences --> NULL
+        //default in Houses --> NULL
+        private function shared_pay($attr, $pref){
+            if ( isset($pref) && ($pref) ){
+                if ( (!isset($attr)) || (!($attr)) ){
+                    return true;
+                }else{
+                    return false;
+                }
             }else{
-                return false;
+                return true;    
+            } 
+        }
+
+  
+        //default in Preferences --> 0
+        //default in Houses --> NULL
+        private function has_photo($attr, $pref){
+            if ($pref){
+                if( isset($attr) && ($attr) ){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return true;    
             }
         }
 
+
+        //refers to optional fields for the owner's house, that have to be greater or equal from the user's relevant preferences (rent_period_min, construction_year_min, bathroom_num_min)
         private function compare_pref_min($attr, $pref){
-            if( ( isset($attr) && !isset($pref) ) || ( !isset($attr) && !isset($pref) ) ){
-                return true;
-            }else if ( isset($attr) && isset($pref) ){
-                if ($attr >= $pref) return true;
-            }else if ( !isset($attr) && isset($pref) ){
+            if( isset($pref) ){
+              if( isset($attr) && ($attr >= $pref) ){
+                return true;    
+              }else{
                 return false;
+              }
+            }else{
+                return true;    
             }
         }
 
+        //refers to compulsory fields for the owner's house, that have to be less or equal from the user's relevant preferences (area_max, price_max)
         private function compare_max($attr, $pref){
-            if( isset($pref) && ($attr <= $pref) ){
+            if( isset($pref) ){
+                if ($attr <= $pref) return true;
+                else return false;
+            }else{   
                 return true;
-            }else{
-                return false;
             }
         }
 
+        //refers to compulsory fields for the owner's house, that have to be greater or equal from the user's relevant preferences (area_min, bedroom_num_min, floor_id_min)
         private function compare_min($attr, $pref){
-            if( isset($pref) && ($attr >= $pref) ){
+            if( isset($pref) ){
+                if ($attr >= $pref) return true;
+                else return false;
+            }else{   
                 return true;
-            }else{
-                return false;
             }
         }
 
+        //refers to compulsory fields for the owner's house, that have to be equal with the user's relevant preferences (house_type_id, heating_type_id)
+        //default in Preferences --> 0
         private function compare_equal($attr, $pref){
             if( $pref == 0  || $attr == $pref ){
                 return true;
@@ -141,20 +194,46 @@ class EmailShell extends Shell{
             }
         }
 
+        //refers to compulsory fields for the owner's house, that have to be equal with the user's relevant preferences (municipality_id)
+        //default in Preferences --> NULL
         private function compare_equal_null($attr, $pref){
-            if( !isset($pref)  || ( isset($pref) && $attr == $pref ) ){
-                return true;
+            if(isset($pref)){
+                if($pref == $attr){
+                    return true;    
+                }else{
+                    return false;
+                }
             }else{
-                return false;
+                return true;    
             }
         }
 
-        private function shared_pay($attr, $pref){
-            if ( isset($pref) && ($attr == 1) ){
-                return true;
-            }else{
-                return false;    
+
+        function email($email_all){
+            //import classes
+            App::import('Core', 'Controller');
+            App::import('Component', 'Email');
+            //load classes, if loading model associations, components etc is needed
+            $controller =& new Controller();
+            $email =& new EmailComponent(null);
+
+            $email->initialize($controller);
+
+            $email_addr = array_keys($email_all);
+            //pr($email_addr);die();
+
+            //send email
+            $email->from = 'admin@roommates.edu.teiath.gr';
+            $email->subject = 'Ενημέρωση για νέα σπίτια που ταιριάζουν στις προτιμήσεις σας';
+            //$email->sendAs = 'both';
+            for($i=0; $i<count($email_addr); $i++){
+                //$email->set('email', $email_addr[$i]);
+                echo $email_addr[$i];
+                $email->to = $email_addr[$i];
+                $email->send('hey you there!');
+            
             }
         }
+
 }
 ?>
