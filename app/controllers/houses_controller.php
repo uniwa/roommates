76899d776a97faaaabe59746556744153767f289
Thanasis Karetsos <karetsosjr@gmail.com>
2011-11-25 13:54:34 +0200

    delete web service logic from index action

--- app/controllers/houses_controller.php
+++ app/controllers/houses_controller.php
@@ -25,23 +25,6 @@ class HousesController extends AppController {
             return $this->set(compact('houses'));
         }
 
-        if ($this->isWebService()) {
-            if ($this->RequestHandler->isGet()) {
-                $houses = $this->simpleSearch(  $this->getHouseConditions(),
-                                                null, null, false, null,
-                                                $this->getXmlFields(), true);
-                $this->set('houses', $houses);
-                $this->layout = 'xml/default';
-                $this->render('xml/public');
-            } else if ($this->RequestHandler->isPost()) {
-                if (!empty($this->data))
-                    $this->House->save($this->data);
-                $this->set('results', $this->data);
-                $this->layout = 'xml/default';
-                $this->render('xml/create');
-            }
-        }
-
 		$order = array('House.modified' => 'desc');
 		$selectedOrder = 0;
 
