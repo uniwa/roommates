e0492c12825ac1b838ed96e4795c65cf80e27d37
Konstantinos Maragos <kmaragos@gmail.com>
2011-12-06 14:01:31 +0200

    fix copy/paste error

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -336,7 +336,7 @@ class UsersController extends AppController{
         $realestate["RealEstate"]["postal_code"] = $data["RealEstate"]["postal_code"];
         $realestate["RealEstate"]["municipality_id"] = $data["RealEstate"]["municipality_id"];
         $realestate["RealEstate"]["user_id"] = $id;
-        $realestate["RealEstate"]["user_id"] = $data["RealEstate"]["type"];
+        $realestate["RealEstate"]["type"] = $data["RealEstate"]["type"];
 
         if ( $this->RealEstate->save($realestate) === false) {
             return false;
